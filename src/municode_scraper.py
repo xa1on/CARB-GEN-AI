3e6fd4270c907f951787743450c3285da000a55c
xa1on <xalon.dzn@gmail.com>
2025-06-17 13:50:29 -0400

    fix: allow for scraping of municipalities and improved loading

--- src/municode_scraper.py
+++ src/municode_scraper.py
@@ -7,12 +7,14 @@ Authors: Chenghao Li,
 Org: University of Toronto: School of Cities
 """
 
-
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+SNAPSHOTS_DIR = "snapshots"
 
 class MuniCodeCrawler:
     home_url = "https://library.municode.com"
@@ -44,24 +46,32 @@ class MuniCodeCrawler:
         :param self:
         :return:
         """
-        with open("snapshot.html", "w", encoding="utf-8") as f:
+        with open((SNAPSHOTS_DIR + "\\" + str(time.asctime(time.localtime(time.time()))).replace(":", "-") + "-snap.html"), "w", encoding="utf-8") as f:
             f.write(self.browser.page_source)
     
-    def go(self):
+    def go(self, url=None):
         """
-        Goes to current url and waits for loading to finish 
+        Goes to specified url and waits for loading to finish 
 
         :param self:
         :return:
         """
+        if url:
+            self.set_url(url)
         self.browser.get(self.url)
-        buffer_xpath = """/html/body/div[2]/div[2]/ui-view/div/div/div/p/span/i"""
+        buffer_main_xpath = """/html/body/div[2]/div[2]/ui-view/div/div/div/p/span/i""" # main initializing application spinning thing
+        buffer_secondary_xpath = """/html/body/div[2]/div[2]/div/div/span/i""" # secondary loading thing
+        loading_complete_xpath = """/html/body/div[2]/div[2]/div/div/span""" # find hidden loading complete item
         wait = WebDriverWait(self.browser, 2)
-        wait.until(EC.invisibility_of_element_located((By.XPATH, buffer_xpath)))
+        wait.until(EC.invisibility_of_element_located((By.XPATH, buffer_main_xpath)))
+        wait.until(EC.invisibility_of_element_located((By.XPATH, buffer_secondary_xpath)))
+        wait.until(EC.presence_of_element_located((By.XPATH, loading_complete_xpath)))
+        self.take_snapshot()
     
-    def scrape_states(self):
+    def scrape_index_link(self):
         """
-        Scrapes states from page, with state name tied to the link on municode with that states municipalities
+        Scrapes any items with index-link class from page, with name tied to the link.
+        Selecting states and selecting municipalities uses index-link
 
         :param self:
         :return: dictionary in the format {[state_name]: [link to state muni]}
@@ -73,8 +83,13 @@ class MuniCodeCrawler:
 
 def main():
     bob = MuniCodeCrawler()
-    bob.retrieve_html()
-    print(bob.scrape_states())
+    bob.go()
+    states = bob.scrape_index_link()
+    print(states)
+    bob.go(states['california'])
+    muni = bob.scrape_index_link()
+    print(muni)
+
 
 if __name__ == '__main__':
     main()
