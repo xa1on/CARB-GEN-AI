a5606bc3b4aa625097e43e936a66e89f34531693
xa1on <xalon.dzn@gmail.com>
2025-06-18 11:41:59 -0400

    feat: text scraper, outputs in markdown

--- src/municode_scraper.py
+++ src/municode_scraper.py
@@ -78,7 +78,7 @@ class MuniCodeCrawler:
         wait.until(EC.invisibility_of_element_located((By.XPATH, buffer_main_xpath)))
         wait.until(EC.invisibility_of_element_located((By.XPATH, buffer_secondary_xpath)))
         wait.until(EC.presence_of_element_located((By.XPATH, loading_complete_xpath)))
-        self.browser.implicitly_wait(1)
+        self.browser.implicitly_wait(1) # this is not good. we should try and figure out a more foolproof way of ensuring the page loads.
         # self.take_snapshot() # for debugging purposes
     
     def scrape_index_link(self):
@@ -93,16 +93,6 @@ class MuniCodeCrawler:
         items = soup.select("a[class=index-link]")
         return {item.text.lower(): item["href"] for item in items}
     
-    """
-    these are a little redundant but it's ok
-    """
-
-    def scrape_states(self):
-        return self.scrape_index_link()
-    
-    def scrape_cities(self):
-        return self.scrape_index_link()
-
     def scrape_codes(self, depth=0):
         """
         Scrapes codes from municipality page with depth
@@ -121,10 +111,16 @@ class MuniCodeCrawler:
             if depth != 0 or "title" in code_text.lower():
                 result[code_text] = code["href"]
         return result
+    
+    """
+    scrape states and cities are redundant but good for readability
+    """
 
-    """
-    redundant again, but it makes it a litte easier to understand
-    """
+    def scrape_states(self):
+        return self.scrape_index_link()
+    
+    def scrape_cities(self):
+        return self.scrape_index_link()
 
     def scrape_titles(self):
         return self.scrape_codes(0)
@@ -140,34 +136,65 @@ class MuniCodeCrawler:
         Scrapes text from code on page
 
         :param self:
-        :return: string of the output
+        :return: string of the output in markdown format
         """
+        def text_selector(tag):
+            # only select text with h2, h3, or p tags
+            return tag.name == "h2" or tag.name == "h3" or tag.name == "p"
 
-
+        result = ""
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
+        text_block = soup.select_one("ul.chunks.list-unstyled.small-padding") # contains the text
+        text = text_block.find_all(text_selector)
+        previous_line_incr = 0
+        for line in text:
+            if line.name == "h2" or line.name == "h3":
+                result += "#" * int(line.name[1]) + ' ' + line.select_one("div[class=chunk-title]").text + '\n'
+            else:
+                # splitting to remove all trailing white-space
+                split = line.text.split('\n')
+                insert_text = ""
+                for text_line in split:
+                    insert_text += text_line.strip() + (' ' if len(text_line) > 0 else '')
+                if line.has_attr("class") and "incr" in line.get("class")[0]:
+                    current_line_incr = int(line.get("class")[0][-1:]) + 1
+                    insert_text = ' ' * 4 * (current_line_incr - 1) + insert_text
+                    previous_line_incr = current_line_incr + 1
+                else:
+                    insert_text += '\n'
+                    if previous_line_incr:
+                        previous_line_incr = 0
+                result += insert_text
+        #with open("test.md", "w", encoding="utf-8") as f:
+            #f.write(result) # for testing purposes
+        return result
     
 
 def main():
-    bob = MuniCodeCrawler()
-    bob.go() # goes to home page of municode
-    states = bob.scrape_states() # gets a dict of states
+    muni_scraper = MuniCodeCrawler()
+    muni_scraper.go("https://library.municode.com/ca/milpitas/codes/code_of_ordinances?nodeId=TITXIZOPLAN_CH4CUREPRPR")
+    print(muni_scraper.scrape_text())
+    """
+    muni_scraper.go() # goes to home page of municode
+    states = muni_scraper.scrape_states() # gets a dict of states
     print(states)
-    bob.go(states["california"]) # goes to california via the results of states
-    muni = bob.scrape_cities() # gets a dict of municipalities in the state of california
+    muni_scraper.go(states["california"]) # goes to california via the results of states
+    muni = muni_scraper.scrape_cities() # gets a dict of municipalities in the state of california
     print(muni)
-    bob.go(muni["tracy"]) # goes to tracy
-    titles = bob.scrape_titles() # grabs all the titles for tracy
+    muni_scraper.go(muni["tracy"]) # goes to tracy
+    titles = muni_scraper.scrape_titles() # grabs all the titles for tracy
     print(titles)
-    bob.go(titles["Title 5 - SANITATION AND HEALTH"]) # scrapes the chapters in title 5
-    more_chapters = bob.scrape_chapters()
+    muni_scraper.go(titles["Title 5 - SANITATION AND HEALTH"]) # scrapes the chapters in title 5
+    more_chapters = muni_scraper.scrape_chapters()
     print(more_chapters)
-    bob.go(titles["Title 4 - PUBLIC WELFARE, MORALS AND CONDUCT"]) # access title 4 for tracy
-    chapters = bob.scrape_chapters() # scrapes the chapters in title 4
+    muni_scraper.go(titles["Title 4 - PUBLIC WELFARE, MORALS AND CONDUCT"]) # access title 4 for tracy
+    chapters = muni_scraper.scrape_chapters() # scrapes the chapters in title 4
     print(chapters)
-    bob.go(chapters["Chapter 4.12 - MISCELLANEOUS REGULATIONS"]) # access chapter
-    articles = bob.scrape_articles() # scrapes the articles
+    muni_scraper.go(chapters["Chapter 4.12 - MISCELLANEOUS REGULATIONS"]) # access chapter
+    articles = muni_scraper.scrape_articles() # scrapes the articles
     print(articles)
-    bob.go(articles["Article 14. - Soliciting and Aggressive Solicitation"])
-
+    muni_scraper.go(articles["Article 14. - Soliciting and Aggressive Solicitation"]) # access chapter's article
+    print(muni_scraper.scrape_text()) # scrapes all text from article"""
 
 
 
