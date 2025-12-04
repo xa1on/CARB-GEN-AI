c15083b2bae9c0b1a86b88696cdc4faef6c0ec2b
Bo Wang <bo.wang@berkeley.edu>
2025-12-04 13:16:35 -0800

    Updated RAG Pipeline to delete all information relevant to query after each use.

--- src/Rag/rag_poc.py
+++ src/Rag/rag_poc.py
@@ -11,10 +11,9 @@ import google.generativeai as genai
 
 from langchain_community.document_loaders import UnstructuredMarkdownLoader # loads markdown files and converts into Langchain document
 from langchain_text_splitters import RecursiveCharacterTextSplitter # splits long documents into smaller chunks (pargarphs, sentences - instead of mid word)
-from langchain_community.vectorstores import Chroma # vector database for storing embeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings  # Use old one
 from langchain_google_genai import ChatGoogleGenerativeAI # langchain wrapper for gemini
-from langchain.chains import RetrievalQA #Combines retriveal (search) + question answering into one chain (will switch later to conversationalRetricealChain)
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Load environment variables
 load_dotenv()
@@ -55,32 +54,42 @@ text_splitter = RecursiveCharacterTextSplitter(
 chunks = text_splitter.split_documents(documents)
 print(f" Split into {len(chunks)} chunks")
 
-
-# --- PHASE 2: CREATE EMEDDINGS & VECTORE STORE ---
+question = "Does Campbell have just cause eviction policies?"
+print(f"Question: {question}")
 
 # Create embeddings
 print(f"\n Creating embeddings ...")
 embeddings = HuggingFaceEmbeddings(
     model_name="all-MiniLM-L6-v2"
 )
+ind_vectors = embeddings.embed_documents([c.page_content for c in chunks])
+
+query_vec = embeddings.embed_query(question)
+
+# now use cosine similarity for custom, non-database-based comparisons
+similarities = cosine_similarity([query_vec], ind_vectors)[0]
+top_k = 3
+top_indices = similarities.argsort()[-top_k:][::-1]
+
+retrieved_chunks = [chunks[i] for i in top_indices]
 
 # Create ChromaDB vector store
-print(f"\n Storing vectors in ChromaDB...")
-vectorstore = Chroma.from_documents(
-    documents=chunks,
-    embedding=embeddings,
-    persist_directory="./chroma_db" # Saves to disk!
-)
-print(f" Vector store created with {len(chunks)} chunks")
+# print(f"\n Storing vectors in ChromaDB...")
+# vectorstore = Chroma.from_documents(
+#    documents=chunks,
+#    embedding=embeddings,
+#    persist_directory="./chroma_db" # Saves to disk!
+#)
+# print(f" Vector store created with {len(chunks)} chunks")
 
 
 # --- PHASE 3: QUERY & ANSWER ---
 
 # Create retriever from vector store
-print(f" Setting up retriever...")
-retriever = vectorstore.as_retriever(
-    search_kwargs={"k": 3} # Return top 3 most relevant chunkcs (can update this later )
-)
+# print(f" Setting up retriever...")
+# retriever = vectorstore.as_retriever(
+#    search_kwargs={"k": 3} # Return top 3 most relevant chunkcs (can update this later )
+# )
 
 # Create Gemini LLM
 print(f"\n Initializing Gemini...")
@@ -91,27 +100,41 @@ llm = ChatGoogleGenerativeAI(
 )
 
 #Create RAG chain (combines retrieval + question answering)
-print(f"\n Building RAG chain...")
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff", # "stuff" = put all chunks in one prompt
-    retriever=retriever,
-    return_source_documents=True # Return which chunks were used
-)
+# print(f"\n Building RAG chain...")
+# qa_chain = RetrievalQA.from_chain_type(
+#    llm=llm,
+#    chain_type="stuff", # "stuff" = put all chunks in one prompt
+#    retriever=retriever,
+#    return_source_documents=True # Return which chunks were used
+# )
 
-question = "Does Campbell have just cause eviction policies?"
-print(f"Question: {question}")
+context = "\n\n".join([c.page_content for c in retrieved_chunks])
+# handbuild our new prmopt instead of just having pileline taking are of everything
+prompt = f"""
+Given the following municipal ordinance context to answer the question.
 
-print(f"\n Searching vector database...")
-result = qa_chain.invoke({"query": question})
+Context:
+{context}
 
-print(f"\n Answer:")
-print(result["result"])
-
-print(f"\n Sources used: {len(result['source_documents'])} chunks")
-for i, doc in enumerate(result['source_documents']):
-    print(f"\nChunk {i+1} preview")
-    print(doc.page_content[:200]+ "...")
+Question: {question}
+"""
+response = llm.invoke(prompt)
+print("\n Answer:")
+print(response)
 
 
+# print(f"\n Searching vector database...")
+# result = qa_chain.invoke({"query": question})
+
+# print(f"\n Answer:")
+# print(result["result"])
+
+# print(f"\n Sources used: {len(result['source_documents'])} chunks")
+# for i, doc in enumerate(result['source_documents']):
+#    print(f"\nChunk {i+1} preview")
+#    print(doc.page_content[:200]+ "...")
+
+del chunks
+del ind_vectors
+del retrieved_chunks
     
