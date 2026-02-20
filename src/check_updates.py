fd2b4474335871ac8b22c9db8873214bfad6e979
cli <82491956+xa1on@users.noreply.github.com>
2026-02-20 15:46:26 -0800

    refactor: ease of use constants for check_updates

--- src/check_updates.py
+++ src/check_updates.py
@@ -5,8 +5,9 @@ from .scrapers import municode_scraper
 import csv
 
 INPUT_CSV = "data/datasets/2025 ARB Policy Map Ordinance Table - Copy of Master (Auto-Updates).csv"
-
 OUTPUT_FILE = "logs/updates.txt"
+VERBOSE = False # false means it only outputs the names of the city-policy pairs that require updates, true means it logs every city policy pair it checks in addition to a requires update tag next to the ones that require updates.
+RUN_ALL = False # false means it inputs the user for a specific city to check while true starts checking every city in the database
 
 def clear():
     open(OUTPUT_FILE, "w", encoding="utf-8").close()
@@ -22,7 +23,8 @@ def check(agent: municode_scraper.MuniCodeScraper, city, county, policy_type, ex
     if "library.municode.com" not in link:
         return False
     agent.go(link)
-    # log(f"Checking: {city}: {policy_type} - {number}\n")
+    if VERBOSE:
+        log(f"Checking: {city}: {policy_type} - {number}\n")
     result = agent.scrape_changes(stop=municode_scraper.Date.from_string(date_checked), max_dates=1)
     if len(result):
         return result
@@ -38,7 +40,7 @@ def check_muni(csv_inp: str, agent: municode_scraper.MuniCodeScraper, muni: str,
                     line[11] = override_date.to_string()
                 checking = check(agent, *line)
                 if checking:
-                    log(f"REQUIRES UPDATE: {line[0]}: {line[2]} - {line[4]} ({checking[0]})\n")
+                    log(f"{"REQUIRES UPDATE: " if VERBOSE else ""}{line[0]}: {line[2]} - {line[4]} ({checking[0]})\n")
 
 def check_all(csv_inp: str, agent: municode_scraper.MuniCodeScraper, override_date: municode_scraper.Date|None=None):
     with open(csv_inp, mode='r', encoding="utf8") as file:
@@ -49,7 +51,7 @@ def check_all(csv_inp: str, agent: municode_scraper.MuniCodeScraper, override_da
                     line[11] = override_date.to_string()
                 checking = check(agent, *line)
                 if checking:
-                    log(f"REQUIRES UPDATE: {line[0]}: {line[2]} - {line[4]} ({checking[0]})\n")
+                    log(f"{"REQUIRES UPDATE: " if VERBOSE else ""}{line[0]}: {line[2]} - {line[4]} ({checking[0]})\n")
 
 def run_all():
     agent = municode_scraper.MuniCodeScraper()
@@ -68,4 +70,7 @@ def run_muni():
 
 if __name__ == "__main__":
     clear()
-    run_muni() # replace with run_all to run checker on all municipalities
+    if RUN_ALL:
+        run_all()
+    else:
+        run_muni() # replace with run_all to run checker on all municipalities
